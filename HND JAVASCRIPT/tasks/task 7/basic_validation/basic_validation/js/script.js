d5108ef5c1c19ccc5ed7606417a3168541b098ec
Ant1ing <cameronjenkins1412@gmail.com>
2023-09-14 11:34:44 +0100

    i messed up the code lol, fixed it now

--- HND JAVASCRIPT/tasks/task 7/basic_validation/basic_validation/js/script.js
+++ HND JAVASCRIPT/tasks/task 7/basic_validation/basic_validation/js/script.js
@@ -1,16 +1,16 @@
-let logIn = document.getElementById('btn');
-logIn.addEventListener('click', validate);
+let btn = document.getElementById('btn');
+btn.addEventListener('click', validate);
 
 function validate() {
     
     let userName = document.getElementById('uName');
-    let pwd = doument.getElementById('pwd');
+    let pwd = document.getElementById('pwd');
 
     if (userName.value === '' && pwd.value === '') {
-        let error = document.querySelector('.error');
-        error.innerHTML = "The Fields Cant Be Empty";
+        let error = document.querySelector('error');
+        error.innerText = "The Fields Cant Be Empty";
 
-    } else if (userName.value === 'admin' && Number (pwde.value) === 123) {
+    } else if (userName.value === 'admin' && Number (pwd.value) === 123) {
         window.location.href = 'loggedIn.html';
     } else {
         window.location.href = 'logInDenied.html';
